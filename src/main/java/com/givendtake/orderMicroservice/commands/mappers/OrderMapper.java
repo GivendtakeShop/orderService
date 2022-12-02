b09f4aad05f625d48af08ca2561b5768a33c1fac
gitRedDev <redouane_karzazi@um5.ac.ma>
2022-12-02 15:01:37 +0100

    put productOrderCommandToProductOrder function inside ordermapper

--- src/main/java/com/givendtake/orderMicroservice/commands/mappers/OrderMapper.java
+++ src/main/java/com/givendtake/orderMicroservice/commands/mappers/OrderMapper.java
@@ -1,8 +1,11 @@
 package com.givendtake.orderMicroservice.commands.mappers;
 
 import com.givendtake.orderMicroservice.commands.OrderCommand;
+import com.givendtake.orderMicroservice.commands.ProductOrderCommand;
 import com.givendtake.orderMicroservice.entities.Order;
+import com.givendtake.orderMicroservice.entities.Product;
 import com.givendtake.orderMicroservice.entities.ProductOrder;
+import com.givendtake.orderMicroservice.services.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,14 +16,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderMapper {
 
-    private final ProductOrderMapper productOrderMapper;
+    private final ProductService productService;
+
+    public ProductOrder productOrderCommandToProductOrder(ProductOrderCommand productOrderCommand, Order order){
+        Product p = productService.findById(productOrderCommand.getProductId());
+        return ProductOrder.builder()
+                .order(order)
+                .product(p)
+                .productQuantity(productOrderCommand.getProductQuantity())
+                .build();
+    }
 
     public Order orderCommandToOrder(OrderCommand orderCommand){
         Order order = new Order();
 
         List<ProductOrder> productOrders =
                 orderCommand.getProductOrders()
-                        .stream().map(e -> productOrderMapper.productOrderCommandToProductOrder(e,order))
+                        .stream().map(e -> productOrderCommandToProductOrder(e,order))
                         .toList();
 
         order.setProductOrders(productOrders);
