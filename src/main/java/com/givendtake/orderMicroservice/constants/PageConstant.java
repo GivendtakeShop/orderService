eb8ee74c10760074f57035085c6274942553ce86
KARZAZI REDOUANE <76531404+gitRedDev@users.noreply.github.com>
2022-12-06 02:00:40 +0100

    feat[GS-33] : filterOrders (#6)
    * feat[GS-33] : filterOrders
    
    * updating tests to fit changes

---
+++ src/main/java/com/givendtake/orderMicroservice/constants/PageConstant.java
@@ -0,0 +1,6 @@
+package com.givendtake.orderMicroservice.constants;
+
+public class PageConstant{
+    public static final String PAGE_NB = "0";
+    public static final String PAGE_SIZE = "5";
+}
--- src/main/java/com/givendtake/orderMicroservice/repositories/OrderRepository.java
+++ src/main/java/com/givendtake/orderMicroservice/repositories/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.givendtake.orderMicroservice.repositories;
 
 import com.givendtake.orderMicroservice.entities.Order;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order,String> {
     Page<Order> findAll(Pageable pageable);
+
+    Page<Order> findAll(Example example, Pageable pageable);
 }
